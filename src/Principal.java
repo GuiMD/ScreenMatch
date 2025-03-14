240003a17cf06805c0b20ba07a68e3a1b50b6176
Guilherme.MD <gui.martins.duarte@gmail.com>
2025-03-13 22:34:36 -0300

    Added getDuracaoEmMinutos() method in the 'Serie' subclass with method overriding. New implementations and fixed issues in the 'Principal' class.

--- src/Principal.java
+++ src/Principal.java
@@ -1,21 +1,25 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
 
     public static void main(String[] args) {
-        Filme favorito = new Filme();
 
-        favorito.setNome("The Matrix");
-        favorito.setAnoDeLancamento(1999);
-        favorito.setDuracaoEmMinutos(135);
-        favorito.setIncluidoNoPlano(true);
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
-        favorito.exibeFichaTecnica();
-        favorito.avalia(9);
-        favorito.avalia(8);
-        favorito.avalia(9);
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração do filme: " + lost.getDuracaoEmMinutos());
 
-        System.out.println("Média de avaliações do filme: " +favorito.pegaMedia());
     }
 
 }
